9d3c842dab943c38c9b30ec0f75641b02af6dea8
YuguangIpsen <yuguang.fan@gmail.com>
2021-10-20 19:11:28 +1100

    implement styles with whirlwind

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,54 @@
 import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
 import TopicCard from "../components/topicCard";
+import BigButton from "../components/bigButton";
+
 export default function Home() {
 	return (
-		<div>
+		<div className='container mx-auto w-full'>
 			<Head>
 				<title>Science Fund</title>
 				<meta name='description' content='Science Fund Home Page' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
-			<main className={styles.container}>
-				<section className={styles.landing}>
-					<p className={styles.slogan}>
+			<main className='container mx-auto'>
+				<section className='flex flex-col items-center justify-center  bg-dark-water text-white h-screen w-screen '>
+					<p className='text-grey-800 text-2xl italic mt-34 mb-4 '>
 						Reimagining the path to discovery
 					</p>
-					<h1 className={styles.title}>science fund</h1>
+					<h1 className='text-grey-900 text-6xl uppercase mb-6'>
+						science fund
+					</h1>
 
-					<div className={styles.grid}>
-						<div className={styles.button}>
-							<Link href='/donate'>
-								<h2>Donate</h2>
-							</Link>
-						</div>
-						<div className={styles.button}>
-							<Link href='/trace'>
-								<h2>Trace</h2>
-							</Link>
-						</div>
+					<div className='flex flex-row justify-between '>
+						<BigButton label='Donate' />
+						<BigButton label='Trace' />
 					</div>
 				</section>
 				{/* Curated Pool */}
-				<section className={styles.curate}>
-					<h1 className={styles.sectionTitle}>
-						Contribute to the funding pools{" "}
-					</h1>
-					<div className={styles.grid}>
+				<section className='container mx-auto px-4'>
+					<div className='container py-10 px-10'>
+						<h1 className='text-3xl my-5'>
+							Curated Funding Pools{" "}
+						</h1>
+						<p className='text-base my-3'>
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit, sed do eiusmod tempor incididunt ut labore et
+							dolore magna aliqua. Ut enim ad minim veniam, quis
+							nostrud exercitation ullamco laboris nisi ut aliquip
+							ex ea commodo consequat. Duis aute irure dolor in
+							reprehenderit in voluptate velit esse cillum dolore
+							eu fugiat nulla pariatur. Excepteur sint occaecat
+							cupidatat non proident, sunt in culpa qui officia
+							deserunt mollit anim id est laborum
+						</p>
+
+						<button className='bg-gray-900 text-white hover:bg-gray-700 py-2 px-4 rounded my-5'>
+							<h2>Connect wallet to donate</h2>
+						</button>
+					</div>
+
+					<div className='flex flex-row justify-evenly'>
 						<TopicCard
 							imagePath='/images/topics/brain.jpg'
 							topic='Neuroscience'
@@ -61,7 +72,7 @@ export default function Home() {
 					</div>
 				</section>
 			</main>
-			<footer className={styles.footer}>
+			<footer className='flex flex-row justify-between my-2'>
 				<a>@ 2021 science fund dao</a>
 				<a href='#'>white paper</a>
 			</footer>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	purge: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			backgroundImage: {
+				"dark-water": "url('/images/header-bg.jpg')",
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
