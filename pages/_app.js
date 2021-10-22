d63a013912cca86295e97d21e49a8602589b82ba
Amirjab21 <30866671+Amirjab21@users.noreply.github.com>
2021-10-22 18:42:04 +0100

    ability to connect metamask wallet to science fund implemented

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
+import { Web3Provider } from '@ethersproject/providers';
+import { Web3ReactProvider } from '@web3-react/core';
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	
+	function getLibrary(provider) {
+		const library = new Web3Provider(provider);
+		library.pollingInterval = 12000;
+		return library;
+	}
+
+	return (
+			<Web3ReactProvider getLibrary={getLibrary}>
+				<Component {...pageProps} />
+			</Web3ReactProvider>
+	);
 }
 
 export default MyApp;
