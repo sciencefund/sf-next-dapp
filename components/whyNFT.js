cc62eda6577ec6c416fb90a4d9df6412b5dd522a
Yuguang Ipsen <yuguang.fan@gmail.com>
2021-11-09 17:04:11 +1100

    upgrade the frontpage content

---
+++ components/whyNFT.js
@@ -0,0 +1,22 @@
+
+export default function WhyNFT() {
+
+    return (
+        <section className='relative mx-auto px-4 py-10 '>
+            <div className='py-10 px-10'>
+                <div className="">
+                    <h1 className='text-3xl text-white mt-20 text-center font-bold font-sans uppercase'>
+                        You may ask, Why NFTs?
+                        {" "}
+                    </h1>
+                    <h5 className="text-base text-center text-gray-400 my-3 italic "> unleashing the power of NFTs beyond jpegs</h5>
+                </div>
+
+                <p className='mx-auto text-center text-base italic text-gray-300 h-96 my-10 md:w-1/2  sm:w-3/4'>
+                    Because NFTs are awesome!! On a more serious note, we see the potential of the tokenised nature of NFT as a fundamental building block connecting the entire stack for science research, from funding to publication and back, pushing humanity forward.
+                </p>
+
+            </div>
+        </section >
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,13 @@ import { BigNumber } from "@ethersproject/bignumber";
 
 import ScienceFund from "../artifacts/contracts/ScienceFund.sol/ScienceFund.json";
 
-import TopicCard from "../components/topicCard";
 import BigButton from "../components/bigButton";
 import ConnectWallet from "../components/connectWallet";
 import CheckoutScreen from "../components/checkoutScreen";
-
-
+import Summary from "../components/summary";
+import WhyNFT from "../components/whyNFT";
+import FundingPools from "../components/fundingPool";
+import Trace from "../components/trace";
 
 // contract address on localhost:8545, maybe different for each deployment
 // const contractAddress = process.env.LOCALHOST_CONTRACT_ADDRESS;
@@ -151,99 +152,41 @@ export default function Home() {
 						/>
 					)}
 
-					<div className='text-center text-white mx-auto w-3/4 py-48'>
+					<div className='text-center text-white mx-auto w-3/4 py-48 h-min-96'>
 						<p className='text-grey-800 text-3xl italic mb-4 '>
 							Reimagining the path to discovery
 						</p>
 						<h1 className='text-grey-900 text-6xl uppercase mb-6 font-bold tracking-wide'>
 							science fund
 						</h1>
-						<BigButton
-							label='Donate'
-							href='/'
-						/>
+						<BigButton label="Learn more" />
 					</div>
 				</section>
-				{/* Curated Pool */}
-				<section className='container mx-auto px-4 max-w-6xl '>
-					<div className='container py-10 px-10'>
-						<h1 className='text-3xl my-5'>
-							Curated Funding Pools{" "}
-						</h1>
-						<p className='text-base my-3'>
-							Lorem ipsum dolor sit amet, consectetur adipiscing
-							elit, sed do eiusmod tempor incididunt ut labore et
-							dolore magna aliqua. Ut enim ad minim veniam, quis
-							nostrud exercitation ullamco laboris nisi ut aliquip
-							ex ea commodo consequat. Duis aute irure dolor in
-							reprehenderit in voluptate velit esse cillum dolore
-							eu fugiat nulla pariatur. Excepteur sint occaecat
-							cupidatat non proident, sunt in culpa qui officia
-							deserunt mollit anim id est laborum
-						</p>
+				{startCheckout && <CheckoutScreen
+					close={() => {
+						setStartCheckout(false);
+					}}
+					sftContract={sftContract}
+					account={account}
+				/>}
+
+
+				<div className="w-screen bg-misty-forest bg-opacity-50 bg-cover h-full">
+
+					<Summary />
+					<FundingPools onClick={checkoutScreen} account={account} />
+
+					<Trace onClick={loadUserTokens} account={account} />
+
+
+					<WhyNFT />
+				</div>
 
 
 
-						<button
-							className='bg-gray-900 text-white hover:bg-gray-700 py-2 px-4 rounded my-5'
-							onClick={checkoutScreen}>
-							{account ? (
-								<h2>Mint Tokens</h2>
-							) : (
-								<h2>Connect wallet to donate</h2>
-							)}
-						</button>
 
 
-						{startCheckout && <CheckoutScreen
-							close={() => {
-								setStartCheckout(false);
-							}}
-							sftContract={sftContract}
-							account={account}
-						/>}
 
-
-					</div>
-
-					<div
-						className='flex flex-row justify-evenly
-				items-center'>
-						<TopicCard
-							imagePath='/images/topics/brain.jpg'
-							topic='Neuroscience'
-							description='Can we understand the biological basis of learning, memory, behavior and ultimately consciousness?'
-						/>
-						<TopicCard
-							imagePath='/images/topics/virus.jpg'
-							topic='Infectious Disease'
-							description='What are the chances of a superbug wiping out humanity?'
-						/>
-					</div>
-				</section>
-				{/* Granular Traceability */}
-				<section className='container mx-auto px-4 max-w-6xl '>
-					<div className='container py-10 px-10'>
-						<h1 className='text-3xl my-5'>
-							Granular Traceability{" "}
-						</h1>
-						<p className='text-base my-3'>
-							Lorem ipsum dolor sit amet, consectetur adipiscing
-							elit, sed do eiusmod tempor incididunt ut labore et
-							dolore magna aliqua. Ut enim ad minim veniam, quis
-							nostrud exercitation ullamco laboris nisi ut aliquip
-							ex ea commodo consequat. Duis aute irure dolor in
-							reprehenderit in voluptate velit esse cillum dolore
-							eu fugiat nulla pariatur. Excepteur sint occaecat
-							cupidatat non proident, sunt in culpa qui officia
-							deserunt mollit anim id est laborum
-						</p>
-
-						<button className='bg-gray-900 text-white hover:bg-gray-700 py-2 px-4 rounded my-5'>
-							<h2 onClick={loadUserTokens}>Connect wallet to trace</h2>
-						</button>
-					</div>
-				</section>
 			</div>
 			<footer className='flex flex-row justify-between my-2 mx-2'>
 				<a>@ 2021 science fund dao</a>
