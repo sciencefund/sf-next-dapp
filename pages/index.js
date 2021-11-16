42e7bfe240b9b6d3ae1c59428a336947b5170ec9
Yuguang Ipsen <yuguang.fan@gmail.com>
2021-11-16 14:14:02 +1100

    connect wallet with web3modal

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer, useCallback } from "react";
 
 import Head from "next/head";
 
-import { ethers } from "ethers"
-import { useWeb3React } from "@web3-react/core";
-import { connectors } from "../context/connectors";
+import { ethers, providers } from "ethers"
+import Web3Modal from "web3modal"
 import { BigNumber } from "@ethersproject/bignumber";
+import WalletConnectProvider from '@walletconnect/web3-provider'
+
 
 import ScienceFund from "../artifacts/contracts/ScienceFund.sol/ScienceFund.json";
 
@@ -24,41 +25,125 @@ const contractAddress = process.env.NEXT_PUBLIC_RINKEBY_CONTRACT_ADDRESS;
 console.log(contractAddress);
 
 
-// const NETWORKS = {
-// 	localhost: {
-// 		name: "localhost",
-// 		color: "#666666",
-// 		chainId: 31337,
-// 		blockExplorer: "",
-// 		rpcUrl: "http://" + 'localhost' + ":8545",
-// 	}
-// }
+const providerOptions = {
+	walletconnect: {
+		package: WalletConnectProvider, // required
+		options: {
+			infuraId: "INFURA_ID" // required
+		}
+	}
+}
+
+let web3Modal
+if (typeof window !== 'undefined')
+{
+	web3Modal = new Web3Modal({
+		network: 'mainnet',
+		cacheProvider: true,
+		providerOptions,
+	})
+
+}
 
 
+const initialState = {
+	provider: null,
+	etherProvider: null,
+	address: null,
+	network: null,
+}
 
-export default function Home() {
-	const context = useWeb3React();
-	const { library, account, activate } = context;
+function reducer(state, action)
+{
+	switch (action.type)
+	{
+		case 'SET_WEB3_PROVIDER':
+			return {
+				...state,
+				provider: action.provider,
+				etherProvider: action.web3Provider,
+				address: action.address,
+				network: action.network
+			}
+		case 'SET_ADDRESS':
+			return {
+				...state,
+				address: action.address,
+			}
+		case 'SET_CHAIN_ID':
+			return {
+				...state,
+				network: action.network
+			}
+		case 'RESET_WEB3_PROVIDER':
+			return initialState
+		default:
+			throw new Error()
+	}
 
+}
+
+
+export default function Home()
+{
+
+	const [state, dispatch] = useReducer(reducer, initialState)
+	const { provider, etherProvider, address, network } = state
 
 	const [sftContract, setSftContract] = useState(null);
-	const [localProvider, setLocalProvider] = useState(null);
-
-
 	const [startCheckout, setStartCheckout] = useState(false);
 	const [startTrace, setStartTrace] = useState(false);
 
+	const connect = useCallback(async function ()
+	{
 
+		const provider = await web3Modal.connect()
+		const web3Provider = new providers.Web3Provider(provider)
+		const signer = web3Provider.getSigner()
+		const address = await signer.getAddress()
+		const network = await web3Provider.getNetwork()
 
+		dispatch({
+			type: 'SET_WEB3_PROVIDER',
+			provider,
+			etherProvider: web3Provider,
+			address,
+			network: network.name
+		})
 
-	useEffect(() => {
-		if (!sftContract) {
-			loadContract()
+	}, [])
+
+	const disconnect = useCallback(async function ()
+	{
+		await web3Modal.clearCachedProvider()
+		if (provider?.disconnect && typeof provider.disconnect === 'function')
+		{
+			await provider.disconnect()
 		}
-		if (!account) {
-			activate(connectors.Injected, err => console.log(err))
+		dispatch({
+			type: 'RESET_WEB3_PROVIDER',
+		})
+	}, [provider])
+
+	// auto load cached provider
+	useEffect(() =>
+	{
+		if (web3Modal.cacheProvider)
+		{
+			connect()
 		}
-	});
+	}, [connect])
+
+
+	// useEffect(() => {
+	// 	if (!sftContract) {
+	// 		loadContract()
+	// 	}
+	// 	if (!account) {
+	// 		activate(connectors.Injected, err => console.log(err))
+	// 	}
+	// });
+
 
 
 	// load contract
@@ -66,13 +151,13 @@ export default function Home() {
 
 		if (typeof window.ethereum !== 'undefined') {
 			// load the network provider 
-			const provider = new ethers.providers.Web3Provider(window.ethereum)
-			setLocalProvider(provider);
+			// const provider = new ethers.providers.Web3Provider(window.ethereum)
+			// setLocalProvider(provider);
 
 
 			// connet to contract on the network
-			const contract = new ethers.Contract(contractAddress, ScienceFund.abi, provider);
-			const connectedContract = await contract.connect(provider.getSigner(0));
+			const contract = new ethers.Contract(contractAddress, ScienceFund.abi, web3Provider);
+			const connectedContract = await contract.connect(web3Provider.getSigner(0));
 			setSftContract(connectedContract);
 
 			try {
@@ -99,8 +184,9 @@ export default function Home() {
 		//start checkout screen
 		setStartCheckout(true);
 
-		if (!account) {
-			activate(connectors.Injected, err => console.log(err))
+		if (!address)
+		{
+			connect();
 		}
 	};
 
@@ -108,8 +194,9 @@ export default function Home() {
 		//start trace screen
 		setStartTrace(true);
 
-		if (!account) {
-			activate(connectors.Injected, err => console.log(err))
+		if (!address)
+		{
+			connect();
 		}
 	};
 
@@ -124,17 +211,17 @@ export default function Home() {
 			</Head>
 			<div className='w-screen mx-auto'>
 				<section className='relative mx-auto bg-dark-water bg-fixed bg-cover w-screen'>
-					{account ? (
+					{address ?
 						<ConnectWallet
-							connect={() => checkoutScreen()}
-							label='Mint Tokens'
+							onClick={disconnect}
+							label='Disconnect'
 						/>
-					) : (
+						:
 						<ConnectWallet
-								connect={() => activate(connectors.Injected, err => console.log(err))}
+							onClick={connect}
 							label='Connect Wallet'
 						/>
-					)}
+					}
 
 					<div className='text-center text-white mx-auto w-3/4 py-48 h-min-96'>
 						<p className='text-grey-800 text-3xl italic mb-4 '>
@@ -150,9 +237,9 @@ export default function Home() {
 				<div className="w-screen bg-misty-forest bg-opacity-50 bg-cover h-full">
 
 					<Summary />
-					<FundingPools onClick={checkoutScreen} account={account} />
+					<FundingPools onClick={checkoutScreen} account={address} />
 
-					<Trace onClick={traceScreen} account={account} />
+					<Trace onClick={traceScreen} account={address} />
 
 					<WhyNFT />
 				</div>
@@ -164,7 +251,7 @@ export default function Home() {
 						setStartCheckout(false);
 					}}
 					sftContract={sftContract}
-					account={account}
+					account={address}
 				/>}
 
 				{startTrace && <TraceScreen
@@ -172,7 +259,7 @@ export default function Home() {
 						setStartTrace(false);
 					}}
 					sftContract={sftContract}
-					account={account}
+					account={address}
 				/>}
 
 
